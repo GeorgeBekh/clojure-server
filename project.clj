b1b8b631f37fde54cc0a8bafa4f3e731dc02439e
Georgy Bekh-Ivanov <georgebekh@mail.ru>
2018-05-10 11:48:55 +0300

    init

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject hello-world "0.1.0-SNAPSHOT"
+  :main hello-world.core
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [ring/ring-core "1.6.3"]
+                 [ring/ring-jetty-adapter "1.6.3"]])
